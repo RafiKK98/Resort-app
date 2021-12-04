0561f5cb57c09351e9f546f203c28415d5954bdc
hasib <86922590+Hasib1621@users.noreply.github.com>
2021-12-04 21:04:23 +0600

    Add carousel

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
     <link rel="stylesheet" href="./CSS/style.css">
   </head>
   <body>
+      <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
@@ -66,6 +67,48 @@
         </div>
       </div>
     </nav>
+
+    <!-- Carousel -->
+
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="./Images/banner2.jpg" class="d-block w-100" alt="Banner">
+            <div class="carousel-caption d-none d-md-block">
+                <h1>DuSai Resort & Spa</h1>
+                <p>The most exotic resort in Bangladesh</p>
+            </div>
+            </div>
+            <div class="carousel-item">
+            <img src="./Images/banner3.jpg" class="d-block w-100" alt="Banner">
+            <div class="carousel-caption d-none d-md-block">
+                <h1>DuSai Resort & Spa</h1>
+                <p>The most exotic resort in Bangladesh</p>
+            </div>
+            </div>
+            <div class="carousel-item">
+            <img src="./Images/banner4.jpg" class="d-block w-100" alt="Banner">
+            <div class="carousel-caption d-none d-md-block">
+                <h1>DuSai Resort & Spa</h1>
+                <p>The most exotic resort in Bangladesh</p>
+            </div>
+            </div>
+        </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
     
     <script
