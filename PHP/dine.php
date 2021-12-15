92523742e3ea2fa87a7de1fea5ebe8c54e52d656
RafiKK98 <1830411@iub.edu.bd>
2021-12-16 00:31:12 +0600

    dine php updated

--- PHP/dine.php
+++ PHP/dine.php
@@ -1,29 +1,190 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-      crossorigin="anonymous"
-    />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link rel="stylesheet" href="../CSS/style1.css">
     <title>Dine</title>
 </head>
+
 <body>
-    
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img class="logo" src="../Images/logo (1).png" alt="Logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active nav1" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active nav2" href="#">Rooms and Suite</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link active nav2" href="#">Dine</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active nav2" href="#">Enjoy</a>
+                    </li>
+                </ul>
+                <div class="d-flex">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active nav3" aria-current="page" href="#">Signup</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active nav3" aria-current="page" href="#">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Main Container -->
+    <div class="hotel-pg common">
+        <div class="inner-bg">
+
+        </div>
+        <div class="container">
+            <div class="main">
+                <div class="room-thumb img-left" id="banana-leaf">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="image-holder">
+                                <img src="../Images/banana-thumb.jpg" alt="Banana Thumb">
+                            </div>
+                        </div>
+                        <div class="col-md-6 leaf">
+                            <div class="text-holder">
+                                <h2 class="title">Banana Leaf Restaurant</h2>
+                                <p>
+                                    This is the fine dining restaurant of the resort situated in the
+                                    ground floor of the main hotel building. It serves multiple types
+                                    of Continental, Chinese, Thai & Indian cuisine.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="room-thumb img-right" id="tea-valley">
+                    <div class="row">
+                        <div class="col-md-6 leaf">
+                            <div class="text-holder">
+                                <h2 class="title">Tea Valley Restaurant</h2>
+                                <p>
+                                This half-moon shaped restaurant on the slopes of a small 
+                                hill overlooking the rice field, lake and tea plantations is 
+                                made of wooden frame with thick thatch roof. It features a Show 
+                                Kitchen where cooking is done in front of the guests and serves 
+                                limited varieties of Continental and Bangladeshi cuisines. The 
+                                buffet breakfast is also served here.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="image-holder">
+                                <img src="../Images/tea-valley.jpg" alt="Tea Valley">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="room-thumb img-left" id="forest-pub">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="image-holder">
+                                <img src="../Images/forest-pub.jpg" alt="Forest Pub">
+                            </div>
+                        </div>
+                        <div class="col-md-6 leaf">
+                            <div class="text-holder">
+                                <h2 class="title">Forest Pub</h2>
+                                <p>
+                                The Bar of the resort is a floating platform jutting out 
+                                onto the valley housing the main pool. It serves different 
+                                kinds of cocktails, mocktails, Beers, Red & White Wines. 
+                                Foreign guests are allowed to bring in their own alcoholic 
+                                drinks for consumption in their rooms.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="room-thumb img-right" id="pool-cafe">
+                    <div class="row">
+                        <div class="col-md-6 leaf">
+                            <div class="text-holder">
+                                <h2 class="title">The Pool Cafe</h2>
+                                <p>
+                                This unique sunken pool cafe, the first of its kind in Bangladesh, 
+                                serves snacks, soft drinks and juices to the pool guests.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="image-holder">
+                                <img src="../Images/pool-cafe.jpg" alt="Pool Cafe">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="room-thumb img-left" id="the-garden-lounge">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="image-holder">
+                                <img src="../Images/the-garden-lounge-thumb.jpg" alt="The Garden Lounge">
+                            </div>
+                        </div>
+                        <div class="col-md-6 leaf">
+                            <div class="text-holder">
+                                <h2 class="title">The Garden Lounge</h2>
+                                <p>
+                                A newly made relaxing Lounge, overlooking the 
+                                forest for guests to while away their evening time 
+                                over snacks and hot & cold beverages.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="room-thumb img-right" id="lobby-cafe">
+                    <div class="row">
+                        <div class="col-md-6 leaf">
+                            <div class="text-holder">
+                                <h2 class="title">The Lobby Café</h2>
+                                <p>
+                                    <!-- This is the fine dining restaurant of the resort situated in the
+                                    ground floor of the main hotel building. It serves multiple types
+                                    of Continental, Chinese, Thai & Indian cuisine. -->
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="image-holder">
+                                <img src="../Images/loby-cafe.jpg" alt="Lobby Cafe">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <footer>
-      <p>Niteshwar, Giashnagar, Moulvi Bazar, Bangladesh. Phone: +880 86164100, Hotline: +880 1617005511</p>
+        <p>Niteshwar, Giashnagar, Moulvi Bazar, Bangladesh. Phone: +880 86164100, Hotline: +880 1617005511</p>
     </footer>
 
 
-    
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-      crossorigin="anonymous"
-    ></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
+
 </html>
