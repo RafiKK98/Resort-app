16c6f02b2eb243a89c093ba38681b5cb837461e2
hasib <86922590+Hasib1621@users.noreply.github.com>
2021-12-20 12:48:01 +0600

    login and signup page updated

---
+++ PHP/register.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CSS/register.css">
+    <title>Register Page</title>
+  </head>
+  <body>
+    <section class="h-100 h-custom" style="background-color: #8fc4b7;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-8 col-xl-6">
+        <div class="card rounded-3">
+          <img src="../Images/banner2.jpg" class="w-100" style="border-top-left-radius: .3rem; border-top-right-radius: .3rem;" alt="Sample photo">
+          <div class="card-body p-4 p-md-5">
+            <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 px-md-2">Registration Info</h3>
+
+            <form class="px-md-2">
+
+              <div class="form-outline mb-4">
+                <input type="text" id="form3Example1q" class="form-control" placeholder="Enter the Name"/>               
+              </div>
+
+              <div class="form-outline mb-4">
+                <input type="text" id="form3Example1q" class="form-control" placeholder="Enter the Email"/>               
+              </div>
+
+              <div class="form-outline mb-4">
+                <input type="password" id="form3Example1q" class="form-control" placeholder="Enter the Password"/>               
+              </div>
+
+              <div class="form-outline mb-4">
+                <input type="number" id="form3Example1q" class="form-control" placeholder="Enter the Phone Number"/>               
+              </div>
+
+              <div class="form-outline mb-4">
+                <input type="text" id="form3Example1q" class="form-control" placeholder="Enter the Address"/>               
+              </div>
+
+              
+
+              
+<button type="submit" class="btn btn-success btn-lg mb-1">Submit</button>
+
+              </div>
+
+              
+
+              
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
--- index.php
+++ index.php
@@ -42,10 +42,10 @@
         <div class="d-flex">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active nav3" aria-current="page" href="#">Signup</a>
+              <a class="nav-link active nav3" aria-current="page" href="./PHP/register.php">Signup</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active nav3" aria-current="page" href="#">Login</a>
+              <a class="nav-link active nav3" aria-current="page" href="./PHP/login.php">Login</a>
             </li>
           </ul>
         </div>
