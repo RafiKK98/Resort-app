c7a450f7b24eeddbc32da36c9009d235b37e297d
hasib <86922590+Hasib1621@users.noreply.github.com>
2021-12-22 01:04:59 +0600

    Some styles add

--- PHP/admin.php
+++ PHP/admin.php
@@ -9,14 +9,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Admin Page</title>
-    <link rel="stylesheet" href="../CSS/admin1.css">
+    <link rel="stylesheet" href="../CSS/admin2.css">
   </head>
   <body>
     <h1>ADD ROOM INFO</h1>
     <div class="container">
         <form action="add.php" method="post">
             <div class="row">
-                <div class="card mb-3" style="width: 100%;">
+                <div class="card mb-3 shadow p-3 mb-5 bg-white rounded" style="width: 100%;">
                     <div class="row g-0">
                         <div class="col-md-4">
                             <img src="../Images/room1.jpg" class="img-fluid rounded-start" alt="...">
@@ -39,7 +39,7 @@
             </div>
 
             <div class="row">
-                <div class="card mb-3" style="width: 100%;">
+                <div class="card mb-3 shadow p-3 mb-5 bg-white rounded" style="width: 100%;">
                     <div class="row g-0">
                         <div class="col-md-4">
                             <img src="../Images/room2.jpg" class="img-fluid rounded-start" alt="...">
@@ -62,7 +62,7 @@
             </div>
 
             <div class="row">
-                <div class="card mb-3" style="width: 100%;">
+                <div class="card mb-3 shadow p-3 mb-5 bg-white rounded" style="width: 100%;">
                     <div class="row g-0">
                         <div class="col-md-4">
                             <img src="../Images/room3.jpg" class="img-fluid rounded-start" alt="...">
@@ -85,7 +85,7 @@
             </div>
 
             <div class="row">
-                <div class="card mb-3" style="width: 100%;">
+                <div class="card mb-3 shadow p-3 mb-5 bg-white rounded" style="width: 100%;">
                     <div class="row g-0">
                         <div class="col-md-4">
                             <img src="../Images/room4.jpg" class="img-fluid rounded-start" alt="...">
@@ -108,7 +108,7 @@
             </div>
 
             <div class="row">
-                <div class="card mb-3" style="width: 100%;">
+                <div class="card mb-3 shadow p-3 mb-5 bg-white rounded" style="width: 100%;">
                     <div class="row g-0">
                         <div class="col-md-4">
                             <img src="../Images/room5.jpg" class="img-fluid rounded-start" alt="...">
@@ -131,7 +131,7 @@
             </div>
 
             <div class="row">
-                <div class="card mb-3" style="width: 100%;">
+                <div class="card mb-3 shadow p-3 mb-5 bg-white rounded" style="width: 100%;">
                     <div class="row g-0">
                         <div class="col-md-4">
                             <img src="../Images/room6.jpg" class="img-fluid rounded-start" alt="...">
@@ -154,7 +154,7 @@
             </div>
 
             <div class="row">
-                <div class="card mb-3" style="width: 100%;">
+                <div class="card mb-3 shadow p-3 mb-5 bg-white rounded" style="width: 100%;">
                     <div class="row g-0">
                         <div class="col-md-4">
                             <img src="../Images/room7.jpg" class="img-fluid rounded-start" alt="...">
@@ -177,7 +177,7 @@
             </div>
 
             <div class="row">
-                <div class="card mb-3" style="width: 100%;">
+                <div class="card mb-3 shadow p-3 mb-5 bg-white rounded" style="width: 100%;">
                     <div class="row g-0">
                         <div class="col-md-4">
                             <img src="../Images/room8.jpg" class="img-fluid rounded-start" alt="...">
@@ -200,7 +200,7 @@
             </div>
 
             <div class="row">
-                <div class="card mb-3" style="width: 100%;">
+                <div class="card mb-3 shadow p-3 mb-5 bg-white rounded" style="width: 100%;">
                     <div class="row g-0">
                         <div class="col-md-4">
                             <img src="../Images/room9.jpg" class="img-fluid rounded-start" alt="...">
@@ -222,7 +222,7 @@
                 </div>
             </div>
 
-            <button type="submit" class="btn btn-info  btn-block" style="margin-bottom:10px">Save</button>
+            <button type="submit" class="btn btn-dark  btn-block" style="margin-bottom:10px">Save</button>
 
             </form>
 
